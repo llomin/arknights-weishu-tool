e54fce4dffba346bc948ecc4454125ee046e1c45
llomin <llomin@qq.com>
2026-03-22 02:59:53 +0800

    chore: add vercel analytics

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,4 +1,7 @@
 import { StrategyBoardPage } from '@/pages/strategy-board/StrategyBoardPage';
+import { inject } from '@vercel/analytics';
+
+inject();
 
 export function App() {
   return <StrategyBoardPage />;
