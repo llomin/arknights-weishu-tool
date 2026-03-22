e152d63dbd9544d5ef8895dbe8283ee327d780f8
llomin <llomin@qq.com>
2026-03-22 13:24:48 +0800

    build: add path

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react-swc';
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
+  base: '/arknights-weishu-tool/',
   plugins: [react()],
   resolve: {
     alias: {
